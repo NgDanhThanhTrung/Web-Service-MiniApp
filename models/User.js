af6b3701755e634b226c1427ac408f9dbd0d99df
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-05-19 22:51:23 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    telegramId: { type: String, required: true, unique: true, index: true },
+    username: { type: String, default: 'n/a' },
+    name: { type: String, default: 'Người dùng' },
+    totalCoins: { type: Number, default: 0 },
+    spinsLeft: { type: Number, default: 5 },
+    adsWatchedToday: { type: Number, default: 0 },
+    lastActiveDay: { type: String, default: new Date().toDateString() },
+    refs: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
